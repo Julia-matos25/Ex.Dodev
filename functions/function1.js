d74fa25eda57a9f8cd083617c457a15bf54e7a76
Julia Matos <juliaandrade10matos@gmail.com>
2023-08-03 15:24:49 -0300

    adicionando exercicio

--- functions/function1.js
+++ functions/function1.js
@@ -1,4 +1,20 @@
-var a = parseInt(prompt("Insira um número"))
-var b = parseInt(prompt("Insira um número"))
+var a = parseInt(prompt("Insira o valor de A"))
+var b = parseInt(prompt("Insira o valor de B"))
 
-function 
+function somar(a, b){
+    return a + b;
+}
+
+
+function subtrair(a, b){
+    return a - b;
+}
+
+function multiplicar(a, b){
+    return a * b;
+}
+
+
+function dividir(a, b){
+    return a / b;
+}

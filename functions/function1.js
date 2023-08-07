1ee9056979e7d05f440482675b250b89c0e568ff
Julia Matos <juliaandrade10matos@gmail.com>
2023-08-07 20:28:06 -0300

    edtando exercicio

--- functions/function1.js
+++ functions/function1.js
@@ -1,5 +1,4 @@
-var a = parseInt(prompt("Insira o valor de A"))
-var b = parseInt(prompt("Insira o valor de B"))
+
 
 function somar(a, b){
     return a + b;
@@ -19,8 +18,40 @@ function dividir(a, b){
     return a / b;
 }
 
+var contador = 0;
+var continuar
 
-console.log("Soma:", somar(a, b));
-console.log("Subtração:", subtrair(a, b));
-console.log("Multiplicação:", multiplicar(a, b));
-console.log("Divisão:", dividir(a, b));
+while (continuar == 's'){
+var a = parseInt(prompt("Insira o valor de A"))
+var b = parseInt(prompt("Insira o valor de B"))
+
+
+var operacao = parseInt(prompt("Escolha a operação: Soma/n1 - Subtração/n2 - Multiplicação/n3 - Divisão/n4"))
+ var resultadoOperacao;
+  switch(operacao){
+    case 1: 
+      resultadoOperacao = somar(a, b));
+     break;
+    case 2: 
+      resultadoOperacao = subtrair(a, b));
+     break;
+    case 3: 
+      resultadoOperacao = multiplicar(a, b));
+     break;
+    case 4:  
+      resultadoOperacao = dividir(a, b));
+    default:
+        console.log("Operação inválida")
+     break;
+ }
+
+
+resultado[contador] = resultadoOperacao;
+contador++
+console.log("O resultado da operação é: " + resultadoOperacao);
+
+continuar = prompt("Deseja realizar outra operação? s ou n");
+
+}
+ 
+
